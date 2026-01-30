f9ff1c213d366a5335ac5824b4010114898a3316
linevnikita <mobilnik03@gmail.com>
2026-01-30 20:47:06 +0300

    third

--- assignment.R
+++ assignment.R
@@ -1,7 +1,7 @@
 library(friends)
-library(tidyverse)
+library(tidyverse) 
 library(tidytext)
-library(factoextra) 
+library(factoextra)
 
 top_speakers <- count(friends, speaker, sort = TRUE) |> 
   slice_head(n = 6) |> 
@@ -10,15 +10,16 @@ top_speakers <- count(friends, speaker, sort = TRUE) |>
 friends_tokens <- friends |>
   filter(speaker %in% top_speakers) |>
   unnest_tokens(word, text) |>
-  filter(!str_detect(word, "\\d")) |> 
+  mutate(word = str_remove_all(word, "\\d+")) |>
+  filter(word != "") |>
   select(speaker, word)
 
 friends_tf <- friends_tokens |>
   count(speaker, word) |>
   group_by(speaker) |>
   mutate(tf = n / sum(n)) |>
-  arrange(desc(n), .by_group = TRUE) |>
-  slice_head(n = 500) |> 
+  arrange(-n) |> 
+  slice_head(n = 500) |>
   ungroup() |>
   select(speaker, word, tf)
 
@@ -26,10 +27,8 @@ friends_tf_wide <- friends_tf |>
   pivot_wider(names_from = word, values_from = tf, values_fill = 0) |>
   column_to_rownames("speaker")
 
-friends_scaled <- scale(friends_tf_wide)
-
 set.seed(118)
-km.out <- kmeans(friends_scaled, centers = 3, nstart = 20)
+km.out <- kmeans(scale(friends_tf_wide), centers = 3, nstart = 20)
 
 pca_fit <- prcomp(friends_tf_wide, center = TRUE, scale. = TRUE)
 
@@ -38,8 +37,6 @@ q <- fviz_pca_biplot(
   geom = c("text"),
   habillage = as.factor(km.out$cluster),
   select.var = list(cos2 = 20),
-  repel = TRUE,
+  repel = FALSE,
   ggtheme = theme_minimal()
-) 
-
-
+)
